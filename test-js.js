36da109276fd86c02605024b87b063c20bb18b69
coding-rock <patrickchenuwo@gmail.com>
2020-07-02 23:03:30 -0600

    add test-js file and test the array size

---
+++ test-js.js
@@ -0,0 +1,5 @@
+
+// assign an value to array index that is greater than the array size
+let sampleArray = [0,1,2];
+sampleArray[5] = 5; // the console will add 2 empty slots into the array and then add 5 into the array. so the array size will be 6.
+console.log(sampleArray);
